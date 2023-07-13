9b6defc537c7d5f8c7d874444b5ad0d069c1d36c
andrejdaskalov <41647331+andrejdaskalov@users.noreply.github.com>
2023-07-13 21:57:59 +0200

    navbar-started

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,14 @@
+import Layout from '@/component/layout'
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import '../component/navbar.css'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+    return (
+        <>
+            <Layout>
+                <Component {...pageProps} />
+            </Layout>
+        </>
+    )
 }
