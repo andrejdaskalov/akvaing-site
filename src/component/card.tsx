e76ab9fc6c18f9980b75e7a339fb5b827a638fd4
andrejdaskalov <41647331+andrejdaskalov@users.noreply.github.com>
2023-12-05 23:01:21 +0100

    refactored card to separate component

---
+++ src/component/card.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import  Post  from '../repository/Post'
+
+
+interface CardProps {
+    post: Post,
+    onClick: () => void
+}
+
+export default function Card({post, onClick} : CardProps): JSX.Element {
+    return (
+        <>
+            <div className='card project-card flex-shrink-0 me-1' key={post.id} onClick={onClick}>
+                <img src={post.imageUrls[0]} className='card-img' alt={post.title} />
+                <div className='card-img-overlay d-flex flex-column justify-content-end'>
+                    <h5 className='card-title'>{post.title}</h5>
+                    {/* <p className='card-text'>Some quick example text to build on the card title and make up the bulk of the card&apos;s content.</p>
+                     <a href={'/project/'+ post.id} className="btn btn-outline-secondary stretched-link">Детали</a> */}
+                </div>
+            </div>
+        </>
+    )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,6 +5,8 @@ import styles from '@/styles/Home.module.css'
 import Navbar from '@/component/navbar'
 import Repository from '../repository/repo'
 import { useRouter } from 'next/router'
+import Post from '../component/card'
+import Card from '../component/card'
 
 
 
@@ -35,14 +37,7 @@ export default function Home() {
                     <h2 className='h2 text-dark'>АРХИТЕКТУРА</h2>
                     <div className='card-container d-flex flex-row justify-content-start overflow-auto'>
                         {posts.map(post => (
-                            <div className='card project-card flex-shrink-0 me-1' key={post.id} onClick={() => navigateToProject(post.id)}>
-                                <img src={post.imageUrls[0]} className='card-img' alt={post.title} />
-                                <div className='card-img-overlay d-flex flex-column justify-content-end'>
-                                    <h5 className='card-title'>{post.title}</h5>
-                                    {/* <p className='card-text'>Some quick example text to build on the card title and make up the bulk of the card&apos;s content.</p>
-                                    <a href={'/project/'+ post.id} className="btn btn-outline-secondary stretched-link">Детали</a> */}
-                                </div>
-                            </div>
+                            <Card post={post} onClick={() => navigateToProject(post.id)}/>
                         ))}
                     </div>
                 </div>
@@ -50,13 +45,7 @@ export default function Home() {
                     <h2 className='h2 text-dark'>ХИДРОТЕХНИКА</h2>
                     <div className='card-container d-flex flex-row justify-content-start overflow-auto'>
                         {posts.map(post => (
-                            <div className='card project-card flex-shrink-0 me-1' key={post.id}>
-                                <img src={post.imageUrls[0]} className='card-img' alt={post.title} />
-                                <div className='card-img-overlay d-flex flex-column justify-content-end'>
-                                    <h5 className='card-title'>{post.title}</h5>
-                                    <p className='card-text'>Some quick example text to build on the card title and make up the bulk of the card&apos;s content.</p>
-                                </div>
-                            </div>
+                            <Card post={post} onClick={() => navigateToProject(post.id)}/>
                         ))}
                     </div>
                 </div>
