9d799ec7fd7ea7e8d42b8b778d8ca12940decff3
andrejdaskalov <41647331+andrejdaskalov@users.noreply.github.com>
2023-12-05 22:48:56 +0100

    simplified cards and made them clickable

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,7 @@ import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
 import Navbar from '@/component/navbar'
 import Repository from '../repository/repo'
+import { useRouter } from 'next/router'
 
 
 
@@ -13,6 +14,11 @@ export default function Home() {
 
     const repository = new Repository()
     const posts = repository.getAllPosts()
+    const router = useRouter()
+
+    const navigateToProject = (id: number) => {
+        router.push('/project/' + id)
+    }
 
 
     return (
@@ -29,12 +35,12 @@ export default function Home() {
                     <h2 className='h2 text-dark'>АРХИТЕКТУРА</h2>
                     <div className='card-container d-flex flex-row justify-content-start overflow-auto'>
                         {posts.map(post => (
-                            <div className='card project-card flex-shrink-0 me-1' key={post.id}>
+                            <div className='card project-card flex-shrink-0 me-1' key={post.id} onClick={() => navigateToProject(post.id)}>
                                 <img src={post.imageUrls[0]} className='card-img' alt={post.title} />
                                 <div className='card-img-overlay d-flex flex-column justify-content-end'>
                                     <h5 className='card-title'>{post.title}</h5>
-                                    <p className='card-text'>Some quick example text to build on the card title and make up the bulk of the card&apos;s content.</p>
-                                    <a href={'/project/'+ post.id} className="btn btn-outline-secondary stretched-link">Детали</a>
+                                    {/* <p className='card-text'>Some quick example text to build on the card title and make up the bulk of the card&apos;s content.</p>
+                                    <a href={'/project/'+ post.id} className="btn btn-outline-secondary stretched-link">Детали</a> */}
                                 </div>
                             </div>
                         ))}
